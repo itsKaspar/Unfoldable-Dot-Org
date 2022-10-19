5499fb77dc81a247da237a9875d91dfae58eef95
itsKaspar <kaspar.ravel@gmail.com>
2022-10-19 21:45:14 +0200

    commited things

---
+++ js/ode.js
@@ -0,0 +1,39 @@
+
+___ = "Ode to a Chain of Blocks";
+
+let the; class Block {constructor( of , chain , world ){
+this . next = chain ;  let hands , shackle  = " free " ;
+let blocks , be; this . index = of; this . data = world;
+let tech= " promises "; this.time= new Date().getTime();
+let them = " encrypt " ; this . hash =  $ ( of , chain ,
+this . time ||  true + " ties for a " +  false , world );
+}}let fall = new Block( 0, 0, ___ =" on one "), another;
+let  blocks = [   fall   ] ;  let falling = " further ";
+while ( blocks == fall )  {  let us = " wonder why " ; }
+let it  =  " be asked what "   ;   function  doesItHave(
+                          _
+                          )
+                          {
+let them  = " explain : " , by , building = art = blocks
+[ blocks. length - 1 ], can , be = long = art.index + 1;
+let aura = new Block (be , art.hash, _,be =" capital ");
+while(0){}blocks.push( aura );}makes=false+" fungible ";
+that= " is how it "; function $(hope=that + " is clear "
+,a=_, moment, goes){let weft= " pass ", through , shed =
+single = false + hope + " thread " + goes. toString () +
+a + moment; is= " weaved "; while(0 &&" fabric "){return
+                          0
+                          ;
+                          }
+but="what";if (2>"thread"&& !blocks) {return"together";}
+let two=0,block=single.length, compete = 0;for(aura=0;0;
+null){};if(block=>1&&"longer") while(compete<block)two =
+((two << 5)+aura - two + single. charAt(compete++)) | 0;
+return two;one= undefined;let another,one="incomplete" }
+                          ;
+                          ;
+                          ;
+while(blocks.length<50 && "keep falling"){let me = "ask"
+; let doesItNeed = "not"; doesItHave("another block ?");
+let torn = fabric = "in" + true + " need of" ; console .
+log(blocks[blocks.length - 1]);a="never ending scroll";}
